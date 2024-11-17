807dea6d4558ab9e8654848c5be85c61662b25c6
Marie Zhorova <112802870+mariedelamontagne@users.noreply.github.com>
2024-11-17 18:27:07 +0100

    Nenacitaji se videa z database

--- videos/urls.py
+++ videos/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
-    path('home/', views.home_view, name='home'),
+    path('landingpage/', views.home_view, name='landingpage'),
+    #path('', views.home_view, name='home'),
     path('video_list/', views.video_list, name='video_list'),
    # path('post_comment/<int:video_id>/', views.post_comment, name='post_comment'),
 ]
--- videos/views.py
+++ videos/views.py
@@ -6,10 +6,20 @@ from .models import YouTubeVideo, Comment
 from django.contrib.auth.decorators import login_required
 
 def login_view(request):
-    return render(request, 'login.html')
+    return render(request, 'videos/login.html')
 
 def register_view(request):
-    return render(request, 'register.html')
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            # Redirect to home or another page after login
+            return redirect('home')  # Or any other view name
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'videos/login.html', {'form': form})
 
 def home_view(request):
     return render(request, 'videos/home.html')
